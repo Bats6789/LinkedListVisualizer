52a70d4db0bc3af5c58d8ea2e4151dfa80ed317b
Blake Wingard <bats23456789@gmail.com>
2024-06-06 12:53:12 -0500

    Initialized PyQt6 window.

--- src/main.py
+++ src/main.py
@@ -1,5 +1,18 @@
+"""The main entry point for LinkedListVisualizer
+
+This file is the main entry point for the MazeViewer project.
+"""
+
+import sys
+from PyQt6.QtWidgets import QApplication, QMainWindow
+
+
 def main():
-    print("hello world!")
+    app = QApplication(sys.argv)
+
+    window = QMainWindow()
+    window.show()
+    app.exec()
 
 
 if __name__ == "__main__":
