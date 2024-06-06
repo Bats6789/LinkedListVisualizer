9133d395c88d2404fca09918f2dec2aaee668f99
Blake Wingard <bats23456789@gmail.com>
2024-06-06 13:30:48 -0500

    Added fullscreen and title.

--- src/main.py
+++ src/main.py
@@ -1,16 +1,33 @@
 """The main entry point for LinkedListVisualizer
 
-This file is the main entry point for the MazeViewer project.
+This file is the main entry point for the LinkedListVisualizer project.
 """
 
 import sys
 from PyQt6.QtWidgets import QApplication, QMainWindow
+from PyQt6.QtGui import QKeyEvent
+from PyQt6.QtCore import Qt
+
+
+class MainWindow(QMainWindow):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.setWindowTitle("Linked List Visualizer")
+        self.showFullScreen()
+
+    def keyPressEvent(self, e: QKeyEvent):
+        match e.key():
+            case Qt.Key.Key_Q:
+                self.close()
 
 
 def main():
     app = QApplication(sys.argv)
 
-    window = QMainWindow()
+    app.setStyle('Fusion')
+
+    window = MainWindow()
     window.show()
     app.exec()
 
