489b52fbf64fb006b80f3f2cf885467426395b92
Blake Wingard <bats23456789@gmail.com>
2024-06-18 10:40:23 -0500

    Added the ability to add nodes with 'A'.

--- src/MainWindow.py
+++ src/MainWindow.py
@@ -3,7 +3,6 @@ from PyQt6.QtWidgets import QGraphicsView, QGraphicsScene, QGraphicsItem
 from PyQt6.QtGui import QKeyEvent, QBrush
 from PyQt6.QtCore import Qt
 from NodeItem import NodeItem
-from ArrowItem import ArrowItem
 
 
 class MainWindow(QMainWindow):
@@ -12,6 +11,26 @@ class MainWindow(QMainWindow):
 
         self.setWindowTitle("Linked List Visualizer")
         self.showFullScreen()
+
+        self.nodeCount = 1
+        self.colors = [
+            Qt.GlobalColor.red,
+            Qt.GlobalColor.green,
+            Qt.GlobalColor.cyan,
+            Qt.GlobalColor.lightGray,
+            Qt.GlobalColor.yellow,
+            Qt.GlobalColor.magenta,
+            Qt.GlobalColor.gray,
+            Qt.GlobalColor.white,
+            Qt.GlobalColor.darkBlue,
+            Qt.GlobalColor.darkRed,
+            Qt.GlobalColor.darkGreen,
+            Qt.GlobalColor.darkGray,
+            Qt.GlobalColor.darkCyan,
+            Qt.GlobalColor.darkYellow,
+            Qt.GlobalColor.darkMagenta,
+            Qt.GlobalColor.blue,
+        ]
 
         brush = QBrush(Qt.BrushStyle.SolidPattern)
         brush.setColor(Qt.GlobalColor.black)
@@ -29,32 +48,6 @@ class MainWindow(QMainWindow):
         )
         self.scene.addItem(self.node1)
 
-        brush.setColor(Qt.GlobalColor.blue)
-        self.node2 = NodeItem(0, 0, 120, 100)
-        self.node2.setPos(230, 0)
-        self.node2.setBrush(brush)
-        self.node2.setFlag(QGraphicsItem.GraphicsItemFlag.ItemIsMovable, True)
-        self.node2.setFlag(
-            QGraphicsItem.GraphicsItemFlag.ItemSendsGeometryChanges, True
-        )
-        self.scene.addItem(self.node2)
-
-        start = self.node1.pos()
-        stop = self.node2.pos()
-
-        start.setX(start.x() + self.node1.rect().width())
-        start.setY(start.y() + self.node1.rect().height() / 2)
-        stop.setY(stop.y() + self.node2.rect().height() / 2)
-
-        brush.setColor(Qt.GlobalColor.red)
-        self.arrow = ArrowItem(start, stop)
-        self.arrow.setBrush(brush)
-        self.scene.addItem(self.arrow)
-
-        self.node1.next = self.node2
-        self.node1.startArrow = self.arrow
-        self.node2.stopArrow = self.arrow
-
         self.button = QPushButton()
 
         self.widget = QWidget()
@@ -68,3 +61,14 @@ class MainWindow(QMainWindow):
         match e.key():
             case Qt.Key.Key_Q:
                 self.close()
+            case Qt.Key.Key_A:
+                node = NodeItem(0, 0, 120, 100)
+                node.setPos(500, 500)
+                node.setBrush(self.colors[self.nodeCount % len(self.colors)])
+                self.node1.addNode(node)
+                self.nodeCount += 1
+                node.setFlag(QGraphicsItem.GraphicsItemFlag.ItemIsMovable, True)
+                node.setFlag(
+                    QGraphicsItem.GraphicsItemFlag.ItemSendsGeometryChanges, True
+                )
+                self.scene.addItem(node)
--- src/NodeItem.py
+++ src/NodeItem.py
@@ -1,8 +1,75 @@
 from PyQt6.QtWidgets import QGraphicsRectItem, QGraphicsItem, QGraphicsSceneMouseEvent
-from PyQt6.QtCore import QVariant
+from PyQt6.QtCore import QVariant, QPointF, QRectF
 from PyQt6.QtGui import QCursor
+from typing import Self
+from ArrowItem import ArrowItem
+from math import sqrt
 
 changeType = QGraphicsItem.GraphicsItemChange
+
+
+def distance(a: QPointF, b: QPointF) -> float:
+    tmp = a - b
+    return sqrt(tmp.x() ** 2 + tmp.y() ** 2)
+
+
+def closestPoint(start: QPointF, rect: QRectF) -> QPointF:
+    minDist = 0
+    stop = QPointF(0.0, 0.0)
+    point = QPointF(0.0, 0.0)
+
+    # Top point
+    point.setX(rect.x() + rect.width() / 2)
+    point.setY(rect.y())
+
+    # Top point is always the closest seen so far
+    stop.setX(point.x())
+    stop.setY(point.y())
+    minDist = distance(start, point)
+    print(f'Top ({point.x()}, {point.y()})')
+    print(f'Stop ({stop.x()}, {stop.y()})')
+    print(f'Dist {minDist}')
+
+    # Left point
+    point.setX(rect.x())
+    point.setY(rect.y() + rect.height() / 2)
+    print(f'Left ({point.x()}, {point.y()})')
+
+    tmp = distance(start, point)
+    if minDist > tmp:
+        minDist = tmp
+        stop.setX(point.x())
+        stop.setY(point.y())
+        print(f'Stop ({stop.x()}, {stop.y()})')
+        print(f'Dist {tmp}')
+
+    # right point
+    point.setX(rect.x() + rect.width())
+    point.setY(rect.y() + rect.height() / 2)
+    print(f'Right ({point.x()}, {point.y()})')
+
+    tmp = distance(start, point)
+    if minDist > tmp:
+        minDist = tmp
+        stop.setX(point.x())
+        stop.setY(point.y())
+        print(f'Stop ({stop.x()}, {stop.y()})')
+        print(f'Dist {tmp}')
+
+    # bottom point
+    point.setX(rect.x() + rect.width() / 2)
+    point.setY(rect.y() + rect.height())
+    print(f'Bottom ({point.x()}, {point.y()})')
+
+    tmp = distance(start, point)
+    if minDist > tmp:
+        minDist = tmp
+        stop.setX(point.x())
+        stop.setY(point.y())
+        print(f'Stop ({stop.x()}, {stop.y()})')
+        print(f'Dist {tmp}')
+
+    return stop
 
 
 class NodeItem(QGraphicsRectItem):
@@ -72,14 +139,17 @@ class NodeItem(QGraphicsRectItem):
 
             if self.startArrow is not None:
                 pos = self.pos()
-                pos.setX(pos.x() + self.rect().width())
+                pos.setX(pos.x() + self.rect().width() / 2)
                 pos.setY(pos.y() + self.rect().height() / 2)
+                node = self.next
+                stop = closestPoint(pos, node.mapRectToScene(node.rect()))
                 self.startArrow.start = pos
+                self.startArrow.stop = stop
                 self.startArrow.transformArrow()
 
             if self.stopArrow is not None:
-                pos = self.pos()
-                self.stopArrow.stop = pos
+                stop = closestPoint(self.stopArrow.start, self.mapRectToScene(self.rect()))
+                self.stopArrow.stop = stop
                 pos.setY(pos.y() + self.rect().height() / 2)
                 self.stopArrow.transformArrow()
 
@@ -103,23 +173,45 @@ class NodeItem(QGraphicsRectItem):
         self._next = value
 
     @property
-    def startArrow(self):
+    def startArrow(self) -> ArrowItem:
         """The startArrow property."""
         return self._startArrow
 
     @startArrow.setter
-    def startArrow(self, value):
+    def startArrow(self, value: ArrowItem):
         self._startArrow = value
         if value is not None:
             value.transformArrow()
 
     @property
-    def stopArrow(self):
+    def stopArrow(self) -> ArrowItem:
         """The stopArrow property."""
         return self._stopArrow
 
     @stopArrow.setter
-    def stopArrow(self, value):
+    def stopArrow(self, value: ArrowItem):
         self._stopArrow = value
         if value is not None:
             value.transformArrow()
+
+    def addNode(self, node: Self):
+        head = self
+        count = 1
+        while head.next is not None:
+            count += 1
+            head = head.next
+
+        head.next = node
+        start = head.pos()
+        stop = node.pos()
+
+        start.setX(start.x() + self.rect().width() / 2)
+        start.setY(start.y() + self.rect().height() / 2)
+        stop = closestPoint(start, node.mapRectToScene(node.rect()))
+        arrow = ArrowItem(start, stop)
+        arrow.setBrush(head.brush())
+        self.scene().addItem(arrow)
+
+        head.startArrow = arrow
+        head.next.stopArrow = arrow
+        arrow.transformArrow()
